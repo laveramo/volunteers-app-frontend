eca6b818f7aa5e53798e743f03066f0a823bfba7
luz <laveramo@unal.edu.co>
2024-11-28 11:53:04 +0100

    connect with login service

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-const apiUrl = "http://localhost:8083"; 
+const SU_apiUrl = "http://localhost:8083/api";
+const LI_apiUrl = "http://localhost:8084/api";
 function navigateTo(page) {
     window.location.href = page;
 }
 
 async function signup() {
-    const firstname = document.getElementById('signup-firstname').value;
-    const lastname = document.getElementById('signup-lastname').value;
+    const firstName = document.getElementById('signup-firstname').value;
+    const lastName = document.getElementById('signup-lastname').value;
     const email = document.getElementById('signup-email').value;
-    const phone = document.getElementById('signup-phone').value;
+    const phoneNumber = document.getElementById('signup-phone').value;
     const password = document.getElementById('signup-password').value;
 
-    const response = await fetch(`${apiUrl}/signup`, {
+    console.log(JSON.stringify({ firstName, lastName, email, phoneNumber, password }))
+
+    const response = await fetch(`${SU_apiUrl}/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ firstname, lastname, email, phone, password })
+        body: JSON.stringify({ firstName, lastName, email, phoneNumber, password })
     });
 
-    alert(response.ok ? 'Sign up successful!' : 'Sign up failed!');
-    if (response.ok) navigateTo('index.html');
+    alert(response.ok ? 'Sign up successful!' : 'Sign up failed, try again later');
+    if (response.ok) navigateTo('login.html');
 }
 
 async function login() {
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-    const response = await fetch(`${apiUrl}/login`, {
+    const response = await fetch(`${LI_apiUrl}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
     });
 
     if (response.ok) {
-        const data = await response.json();
-        alert('Login successful! Token: ' + data.token);
-        localStorage.setItem('token', data.token);
         navigateTo('index.html');
     } else {
         alert('Login failed!');
